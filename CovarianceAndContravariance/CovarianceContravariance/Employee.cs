1ee74f8ba1690b193887b1b5cd45f561a737be14
tarcisiocorte <tarcisiocorte@gmail.com>
2017-05-22 09:55:08 -0300

    doing some pratical tests with Covariance and Contravariance

---
+++ CovarianceAndContravariance/CovarianceContravariance/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CovarianceContravariance
+{
+
+    public class Employee
+    {
+        private string m_Name;
+        private int m_ID;
+
+        public Employee(string name, int id)
+        {
+            m_Name = name;
+            m_ID = id;
+        }
+
+        public string Name
+        {
+            get { return m_Name; }
+            set { m_Name = value; }
+        }
+
+        public int ID
+        {
+            get { return m_ID; }
+            set { m_ID = value; }
+        }
+    }
+
+    public class Manager:Employee
+    {
+        public Manager(string name, int id) : base(name, id) { }                      
+    }
+
+    public class BaseComparer<T>: IComparer<T> where T : Employee
+    {       
+            public int Compare(T x, T y)
+            {
+                return (x.ID < y.ID ? 0 : 1);
+            }                                      
+    }      
+}
---
+++ CovarianceAndContravariance/CovarianceContravariance/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovarianceContravariance
+{
+    class Animal { }
+    class Cao : Animal { }
+    delegate T Func1<out T>();
+    delegate void Action1<in T>(T a);
+
+    class Program
+    {
+        public static Cao CreateCaoInstance()
+        {
+            return new Cao();
+        }
+
+        public static void DisplayCaoInstance(Animal animal)
+        {
+            Console.WriteLine("Contravariance :" + animal);
+        }
+        static void Main(string[] args)
+        {
+
+            //Interface Covariance 
+            IMessageRecieved<object> objMessageRX = new DataTX<string>();
+
+            //Interface Contravariance
+            IMessageSend<string> objMessageSend = new DataTX<object>();
+
+
+            //Delegate Covariance 
+            Func1<Cao> delegateCao = new Func1<Cao>(CreateCaoInstance);
+            Cao objCao = delegateCao();
+            Console.WriteLine("Convariance:" + objCao);
+            Func1<Animal> delegateAnimal = delegateCao;
+            Animal objAnimal = delegateAnimal();
+            Console.WriteLine("Covariance:" + objAnimal);
+
+            //Delegate Contravariance
+            Action1<Animal> act1 = new Action1<Animal>(DisplayCaoInstance);
+            act1(new Animal());
+            Action1<Cao> cao1 = act1;
+            cao1(new Cao());
+
+            //Icomparer Contravariance
+            BaseComparer<Manager> objComparer = new BaseComparer<Manager>();
+            List<Manager> managerList = new List<Manager>();
+            managerList.Add(new Manager("Damasceno", 1));
+            managerList.Add(new Manager("Vitor", 2));
+            managerList.Sort(objComparer);
+
+            List<Employee> employeeList = new List<Employee>();
+            employeeList.Add(new Employee("Damasceno", 1));
+            employeeList.Add(new Employee("Vitor", 2));
+
+            //employeeList.Sort(objComparer);
+            managerList.ForEach(e => Console.WriteLine(e.ID + " " + e.Name));
+
+            Console.ReadKey();
+        }
+    }
+
+    interface IMessageRecieved<out T>
+    {
+        T RecievedData();
+    }
+    interface IMessageSend<in T>
+    {
+        void SendData(T data);
+    }
+
+    class DataTX<T> : IMessageSend<T>, IMessageRecieved<T>
+    {
+        private T m_Data;
+
+        #region IMessageSend<T> Members ( Implicit Implementation )
+        void IMessageSend<T>.SendData(T data)
+        {
+            this.m_Data = data;
+        }
+        #endregion
+
+        #region IMessageRecieved<T> Members ( Implicit Implementation )
+
+        public T RecievedData()
+        {
+            return m_Data;
+        }
+        #endregion
+
+        /*
+        public virtual void SendData(T data)
+        {
+            this.m_Data = data;
+        }
+         */
+    }
+}
